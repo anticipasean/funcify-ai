7beb67244befd032378ad17df657fb60263d6804
anticipasean <spmccarron@gmail.com>
2024-05-17 14:08:32 -0400

    Create base setup with spring initializer to explore what latest configurations look like

---
+++ src/main/kotlin/funcify/ai/FuncifyAiApplication.kt
@@ -0,0 +1,11 @@
+package funcify.ai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FuncifyAiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FuncifyAiApplication>(*args)
+}
---
+++ src/test/kotlin/funcify/ai/FuncifyAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package funcify.ai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FuncifyAiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
